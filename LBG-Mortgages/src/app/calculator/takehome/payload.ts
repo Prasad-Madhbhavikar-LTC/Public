34ef04d91fb4fd8ad89ed9e7fc5198e0e5a3c04b
Madhbhavikar <prasad.madhbhavikar@lloydsbanking.dev>
2024-05-16 11:14:48 +0530

    Removed unnecessory folders

--- LBG-Mortgages/src/app/calculator/takehome/payload.ts
+++
@@ -1,7 +0,0 @@
-export interface Payload {
-    gross_income: number;
-    employment_type: string;
-    student_loan_monthly_payment: number;
-    pension_contribution_percentage?: number;
-    pension_contribution_amount?: number;
-}
